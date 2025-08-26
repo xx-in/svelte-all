187dbaf49bdb294970b4ccdc8378e02970562655
徐鑫 <2821550908@qq.com>
2025-08-26 09:30:07 +0800

    test: third party module

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,14 +1,13 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import _ from "lodash"
 
 export const GET: RequestHandler = ({ request }) => {
-	// log all headers
-	console.log(...request.headers);
-
 	// create a JSON Response using a header we received
 	return json({
 		// retrieve a specific header
-		userAgent: request.headers.get('user-agent')
+		userAgent: request.headers.get('user-agent'),
+		test: _.clone("test lodash")
 	}, {
 		// set a header on the response
 		headers: { 'x-custom-header': 'potato' }
