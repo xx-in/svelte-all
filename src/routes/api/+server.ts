f3594c6c6fff25f526d0d6779aa66ccfb0b1aa54
徐鑫 <2821550908@qq.com>
2025-08-26 09:49:33 +0800

    add: favicon and title

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,15 +1,18 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import _ from "lodash"
+import _ from 'lodash';
 
 export const GET: RequestHandler = ({ request }) => {
 	// create a JSON Response using a header we received
-	return json({
-		// retrieve a specific header
-		userAgent: request.headers.get('user-agent'),
-		test: _.clone("test lodash")
-	}, {
-		// set a header on the response
-		headers: { 'x-custom-header': 'potato' }
-	});
+	return json(
+		{
+			// retrieve a specific header
+			userAgent: request.headers.get('user-agent'),
+			test: _.clone('test lodash')
+		},
+		{
+			// set a header on the response
+			headers: { 'x-custom-header': 'potato' }
+		}
+	);
 };
