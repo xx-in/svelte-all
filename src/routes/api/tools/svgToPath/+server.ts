3a48fe436afd00e39dce68200014196accaa64af
徐鑫 <2821550908@qq.com>
2025-08-30 16:32:02 +0800

    fix: 打包异常

--- src/routes/api/tools/svgToPath/+server.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-import opentype from 'opentype.js';
-
-opentype.load('your-font.ttf', (err, font) => {
-	if (err) throw err;
-	const path = font.getPath('x', 0, 0, 72); // (文字, x, y, 字号)
-	const svgPath = path.toSVG(); // 得到 <path d="..."/>
-	return svgPath;
-});
