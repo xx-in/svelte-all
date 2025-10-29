77ea21c0ecdf616d370a488ed095b34375f98e64
徐鑫 <2821550908@qq.com>
2025-10-29 23:23:17 +0800

    add: md

--- src/routes/test/threeTest.svelte
+++ src/routes/test/threeTest.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import type { Light } from "three";
+  import type { Camera, Light } from "three";
   import {
+    AmbientLight,
     BoxGeometry,
     Clock,
     Color,
     DirectionalLight,
+    HemisphereLight,
     Mesh,
     MeshStandardMaterial,
     PerspectiveCamera,
     PointLight,
     Scene,
+    SphereGeometry,
     TextureLoader,
     WebGLRenderer,
   } from "three";
+  import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
   /**
    * 定义全局变量
@@ -27,32 +31,27 @@
   let loops = $state<Record<string, () => void>>({});
   let delta = $state(1000 / 60);
   let seconds = $state(0);
+  let runningStatus = $state(false);
+  let controls = $state<OrbitControls>();
 
   onMount(init);
 
   function createCamera() {
     const camera = new PerspectiveCamera(35, 1, 0.1, 100);
     camera.position.set(0, 0, 20);
-
     return camera;
+  }
+
+  function createControls(camera: Camera, domElement?: HTMLElement | null) {
+    const controls = new OrbitControls(camera, domElement);
+    controls.enableDamping = true;
+    return controls;
   }
 
   function createScene() {
     const scene = new Scene();
     scene.background = new Color("skyblue");
     return scene;
-  }
-
-  function createCube(name: string, postion: [number, number, number]) {
-    const geometry = new BoxGeometry(...postion);
-    const material = new MeshStandardMaterial({
-      color: "white",
-    });
-    const cube = new Mesh(geometry, material);
-    cube.position.set(...postion);
-
-    meshs[name] = cube;
-    return cube;
   }
 
   /**
@@ -83,16 +82,19 @@
 
   function createCube1() {
     const name = "cube1";
-    const cube = createCube(name, [3, 3, 3]);
-    cube.position.set(-7, 0, 0);
-    cube.rotation.set(Math.PI / 4, Math.PI / 4, Math.PI / 4);
-
-    cube.material.map = new TextureLoader().load("/textures/uv-test-col.png");
-    onLoop(name, () => {
-      // 每秒多少度
-      cube.rotation.y += degPerSecond(60);
-      cube.position.x += reverse(10, meterPerSecond(1));
+    const geometry = new BoxGeometry(3, 3, 3);
+    const material = new MeshStandardMaterial({
+      map: new TextureLoader().load("/textures/uv-test-bw.png"),
     });
+    const cube = new Mesh(geometry, material);
+    cube.position.set(0, 0, 0);
+    meshs[name] = cube;
+    // cube.rotation.set(Math.PI / 4, Math.PI / 4, Math.PI / 4);
+    // onLoop(name, () => {
+    // 每秒多少度
+    // cube.rotation.y += degPerSecond(60);
+    //   cube.position.x += reverse(10, meterPerSecond(1));
+    // });
   }
 
   function onLoop(name: string, cb: () => void) {
@@ -100,60 +102,46 @@
     loops[name] = cb;
   }
 
-  function createCube2() {
-    const name = "cube2";
-    const cube = createCube(name, [2, 2, 2]);
-    cube.position.set(0, 2, 0);
-    onLoop(name, () => {
-      cube.rotation.x += 0.03;
-      cube.rotation.y += 0.01;
-      cube.rotation.z += 0.02;
-    });
-  }
-
-  function createLight(name: string, position: [number, number, number]) {
+  function createLight1() {
+    const name = "light1";
+    const position = [-10, 10, -10] as const;
     const light = new PointLight("white", 140);
     light.position.set(...position);
     lights[name] = light;
     return light;
   }
 
+  function createLight2() {
+    const light = new AmbientLight("white", 2);
+    lights["ambientLight"] = light;
+    return light;
+  }
+
+  function createLight3() {
+    const light = new HemisphereLight(
+      "white", // bright sky color
+      "darkslategrey", // dim ground color
+      2, // intensity
+    );
+    lights["hemisphereLight"] = light;
+    return light;
+  }
+
   function createRenderer() {
     const renderer = new WebGLRenderer({
-      antialias: false,
+      antialias: true,
     });
 
     return renderer;
   }
 
   /**
-   * 出时化
+   * 渲染
    */
-  function init() {
-    camera = createCamera();
-    scene = createScene();
-    renderer = createRenderer();
-    createLight("light1", [10, 0, 0]);
-    createLight("light2", [0, 10, 0]);
-    createLight("light3", [0, 0, 10]);
-    createCube1();
-    // createCube2();
-    scene.add(...Object.values(lights), ...Object.values(meshs));
-    container.append(renderer.domElement);
-    resize();
-    renderer.render(scene, camera);
-    onLoop("camera", () => {
-      //   camera!.position.z += meterPerSecond(2);
-    });
-    // 开始运行动画
-    startRunning();
-  }
-
   function render() {
     renderer!.render(scene!, camera!);
   }
 
-  let runningStatus = $state(false);
   /**
    * 监听状态，可以开启动画或者停止动画
    */
@@ -164,10 +152,14 @@
         delta = clock.getDelta();
         seconds += delta;
         Object.values(loops).forEach((cb) => cb());
+        controls?.update();
         render();
       });
     } else {
-      renderer!.setAnimationLoop(null);
+      renderer!.setAnimationLoop(() => {
+        controls?.update();
+        render();
+      });
     }
   });
 
@@ -186,6 +178,37 @@
     camera!.updateProjectionMatrix();
     renderer!.setSize(container.clientWidth, container.clientHeight);
     renderer!.setPixelRatio(window.devicePixelRatio);
+  }
+
+  function createSphere() {
+    const radius = 4;
+    const widthSegments = 16;
+    const heightSegments = 16;
+    const geometry = new SphereGeometry(radius, widthSegments, heightSegments);
+    const material = new MeshStandardMaterial();
+    const sphere = new Mesh(geometry, material);
+    meshs["sphere"] = sphere;
+    return sphere;
+  }
+
+  /**
+   * 初始化
+   */
+  function init() {
+    camera = createCamera();
+    scene = createScene();
+    renderer = createRenderer();
+    createLight1();
+    createLight3();
+    // createCube1();
+    createSphere();
+    // 挂载灯光和模型
+    scene.add(...Object.values(lights), ...Object.values(meshs));
+    container.append(renderer.domElement);
+    resize();
+    controls = createControls(camera, renderer.domElement);
+    // 开始运行动画
+    startRunning();
   }
 </script>
 
